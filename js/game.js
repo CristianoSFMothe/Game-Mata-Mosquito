d8f1de5f72a3aff721668456e56925a3782ae5cc
Cristiano Ferreira <cristianodevsystem@gmail.com>
2021-04-30 19:21:19 -0300

    Criando o elemento HTML de forma programatica

--- js/game.js
+++ js/game.js
@@ -10,9 +10,30 @@ function adjustStageSize() {
 
 adjustStageSize()
 
-var positionX = Math.floor(Math.random() * height)
-var positionY = Math.floor(Math.random() * wight)
+// function positionRandom() {
 
-console.log(positionX, positionY)
+    var positionX = Math.floor(Math.random() * height) - 90
+    var positionY = Math.floor(Math.random() * wight) - 90
+
+    positionX = positionX < 0 ? 0 : positionX
+    positionY = positionY < 0 ? 0 : positionY
+        
+    console.log(positionX, positionY)
+    
+    // Criar o elemento HTML
+    var gnat = document.createElement('img')
+    gnat.src = 'img/gnat.png'
+    gnat.className = 'gnat1'
+    gnat.style.left = positionX + 'px'
+    gnat.style.top = positionY + 'px'
+    gnat.style.position = 'absolute'
+    
+    document.body.appendChild(gnat)
+
+// }]
+
+function sizeRandom() {
+    let class = Math.random()
+}
 
 
